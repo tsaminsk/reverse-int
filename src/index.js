4470d4564e812325dda687b80885e026b71d6033
Talai Sergei <tsa@tut.by>
2020-10-06 15:07:24 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    // return parseInt(Math.abs(n).toString().split('').reverse().join(''))
+    n = Math.abs(n)
+    let hundreds = Math.trunc(n / 100)
+    let tens = Math.trunc(n / 10) % 10
+    return (n % 10) * 100 + tens * 10 + hundreds
 }
